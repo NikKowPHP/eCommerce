291a7d681fa26bf5ea24c31fafbd1d47ba687068
NikKowPHP <nik.kow@outlook.com>
2024-01-06 11:45:09 +0100

    refactor(Index): Update index file with comments and include app layout

--- src/public/index.php
+++ src/public/index.php
@@ -6,9 +6,16 @@ require_once("../app/routes.php");
 
 use App\Router;
 
+// Instantiate the router
 $router = new Router($routes);
 $method = $_SERVER["REQUEST_METHOD"];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Capture the output from the controller method
+ob_start();
 $router->handleRequest($method, $uri);
+$bodyContent = ob_get_clean();
+
+include("../app/Views/layout.php");
 
 ?>
