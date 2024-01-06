98cfd03d62ae7c2049bab33bf5b606e0520db81d
NikKowPHP <nik.kow@outlook.com>
2024-01-06 12:04:36 +0100

    feat(AbstractController): Introduce AbstractController class that is going to define common behavior for controllers

---
+++ src/app/Controllers/AbstractController.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace App\Controllers;
+
+use App\Traits\ViewPathTrait;
+
+abstract class AbstractController
+{
+	use ViewPathTrait;
+	abstract public function index(): void;
+}
--- src/app/Controllers/ProductController.php
+++ src/app/Controllers/ProductController.php
@@ -2,12 +2,8 @@
 declare(strict_types=1);
 namespace App\Controllers;
 
-use App\Traits\ViewPathTrait;
-
-
-class ProductController
+class ProductController extends AbstractController
 {
-	use ViewPathTrait;
 	public function index(): void
 	{
 		$viewPath = __DIR__ . '/../Views/products.php';
