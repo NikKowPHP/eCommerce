43c2077533975b86b04a8504a21e0b3501da45b7
NikKowPHP <nik.kow@outlook.com>
2024-01-06 08:40:07 +0100

    feat(Router): Define basic router stracture to handle requests based on existing routes array

--- src/app/Router.php
+++ src/app/Router.php
@@ -7,6 +7,25 @@ class Router
 	{
 		$this->routes = $routes;
 	}
+	public function handleRequest($method, $uri)
+	{
+		$route = $method .''.$uri;
+		if(array_key_exists($route, $this->routes)) {
+			return $this->handleRoute($this->routes[$route]);
+		}
+		return $this->error404();
+	}
+	protected function handleRoute($handler) 
+	{
+		list($controller, $action) = explode('@',$handler);
+		$controllerInstance = new $controller;
+		return $controllerInstance->$action();
+	}
+	protected function error404()
+	{
+		header($_SERVER["SERVER_PROTOCOL"]. "404 Not Found");
+		echo "404 Not Found";
+	}
 
 
 }
