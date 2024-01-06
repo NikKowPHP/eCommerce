10ee5d083ecdae7237c5b2fac1ea3d998f7ff77b
NikKowPHP <nik.kow@outlook.com>
2024-01-06 08:24:01 +0100

    chore: Install composer with autoload

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,32 @@
+<?php
+class ClassA
+{
+	public function __construct(public int $x, public int $y)
+	{
+	}
+	public function foo():string
+	{
+		return 'foo';
+	}
+	public function bar():object
+	{
+		return new class($this->x, $this->y) extends ClassA {
+			public function __construct(public int $x, public int $y)
+			{
+				parent::__construct($x, $y);
+				echo $this->foo();
+			}
+		};
+	}
+}
+
+?>
+
 <pre>
-	<?php print_r($_SERVER) ?>
+	<?php
+
+$a = new ClassA(2,3);
+print_r($a->bar());
+ ?>
+
 </pre>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8669cc5e82291af5450023b297d543ee62ede7d3',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8669cc5e82291af5450023b297d543ee62ede7d3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
