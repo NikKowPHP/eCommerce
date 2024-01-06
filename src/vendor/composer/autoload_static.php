60e14379cb48eb620128947fbc26b60a3065572d
NikKowPHP <nik.kow@outlook.com>
2024-01-06 09:31:02 +0100

    refactor(composer): Move composer files with vendor folder to the src folder

--- src/vendor/composer/autoload_static.php
+++ src/vendor/composer/autoload_static.php
@@ -9,14 +9,24 @@ class ComposerStaticInit675644e99b7222bba6f7c21c3752ab58
     public static $prefixLengthsPsr4 = array (
         'A' => 
         array (
+            'App\\Models\\' => 11,
+            'App\\Controllers\\' => 16,
             'App\\' => 4,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
+        'App\\Models\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/app/Models',
+        ),
+        'App\\Controllers\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/app/Controllers',
+        ),
         'App\\' => 
         array (
-            0 => __DIR__ . '/../..' . '/src',
+            0 => __DIR__ . '/../..' . '/app',
         ),
     );
 
