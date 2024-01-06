35038ec20c71a54f305169cb054d08134b32fa64
NikKowPHP <nik.kow@outlook.com>
2024-01-06 11:35:10 +0100

    feat(ProductController): Define index method that returns the view with handled not found error

--- src/app/Controllers/ProductController.php
+++ src/app/Controllers/ProductController.php
@@ -1,11 +1,22 @@
 <?php
+declare(strict_types=1);
 namespace App\Controllers;
 
-declare(strict_types=1);
+use App\Exceptions\ViewNotFoundException;
+
 class ProductController
 {
-	public function index():void
+	public function index(): void
 	{
-		echo "displaying all products";
+		try {
+			$viewPath = __DIR__ . '/./Views/products.php';
+			if (!file_exists($viewPath)) {
+				throw new ViewNotFoundException("products.php", $viewPath);
+			}
+			include $viewPath;
+		} catch (ViewNotFoundException $e) {
+			echo $e->getMessage();
+
+		}
 	}
 }
