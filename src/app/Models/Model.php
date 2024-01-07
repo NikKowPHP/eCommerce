df4a1c24afea7acca15017ed55b9942ba83a5b97
NikKowPHP <nik.kow@outlook.com>
2024-01-07 18:30:43 +0100

    feat(Model): Introduce read method that retrieves and instantiates the instanse of the called class

--- src/app/Models/Model.php
+++ src/app/Models/Model.php
@@ -1,7 +1,8 @@
 <?php
-namespace App\Models;
 declare(strict_types=1);
-use App\Database;
+namespace App\Models;
+
+use App\Database\Database;
 
 abstract class Model
 {
@@ -11,24 +12,58 @@ abstract class Model
 	{
 		$this->initDatabase();
 	}
-	protected function initDatabase():void
+	protected function initDatabase(): void
 	{
 		$this->database = new Database();
 	}
-	public function read(int $id): ?self 
+	public function read(int $id): ?static
 	{
 		try {
 			$pdo = $this->database->getConnection();
 
-			$query = "SELECT * FROM " . $this->getTableName(). " WHERE id = :id";
+			$query = "SELECT * FROM " . $this->getTableName() . " WHERE id = :id";
 			$stmt = $pdo->prepare($query);
 			$stmt->bindParam(':id', $id, \PDO::PARAM_INT);
 			$stmt->execute();
-			return $stmt->fetch(\PDO::FETCH_ASSOC) ?:null;
-		} catch(\PDOException $e) {
+			$foundData = $stmt->fetch(\PDO::FETCH_ASSOC) ?: null;
+			if($foundData) {
+				$foundData = $this->convertRow($foundData);
+			}
+			$this->instantiate($foundData);
+			return $this;
+		} catch (\PDOException $e) {
 			echo "Connection failed: " . $e->getMessage();
 			return null;
 		}
 	}
-	abstract protected function getTableName():string;
+	protected function instantiate(array $data): static
+	{
+		foreach ($data as $key => $value) {
+			$setterMethod = 'set' . ucfirst($key);
+			if (method_exists($this, $setterMethod)) {
+				$this->$setterMethod($value);
+			}
+		}
+		return $this;
+	}
+	protected function castProp(string $value): mixed
+	{
+		if (is_numeric($value)) {
+			if ((int) $value == $value) {
+				return (int) $value;
+			} elseif ((float) $value == $value) {
+				return (float) $value;
+			}
+		}
+		return $value;
+	}
+	protected function convertRow(array $row): array
+	{
+		$convertedRow = [];
+		foreach ($row as $key => $value) {
+			$convertedRow[$key] = $this->castProp($value);
+		}
+		return $convertedRow;
+	}
+	abstract protected function getTableName(): string;
 }
