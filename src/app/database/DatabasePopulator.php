996c1b4911faf894e7ca162d9634dfe2a83d4572
NikKowPHP <nik.kow@outlook.com>
2024-01-07 10:05:23 +0100

    feat(DataPopulator): Introduce insertInitialdata method to populate the database with initial data

--- src/app/database/DatabasePopulator.php
+++ src/app/database/DatabasePopulator.php
@@ -40,18 +40,39 @@ class DatabasePopulator
 				"CREATE TABLE IF NOT EXISTS cart_items (
 						id INT AUTO_INCREMENT PRIMARY KEY,
 						cart_id INT NOT NULL,
-						user_id INT NOT NULL,
+						product_id INT NOT NULL,
 						quantity INT DEFAULT 1,
 						FOREIGN KEY (cart_id) REFERENCES carts(id) ON DELETE CASCADE,
 						FOREIGN KEY (product_id) REFERENCES products(id)
 				)",
 			];
-			foreach($queries as $query) {
+			foreach ($queries as $query) {
 				$pdo->exec($query);
 			}
 			echo "Tables created successfully";
+		} catch (\PDOException $e) {
+			echo "Error creating tables: " . $e->getMessage();
+		}
+	}
+	public function insertInitialData(): void
+	{
+		try {
+			$pdo = $this->database->getConnection();
+
+			$insertQueries = [
+				"INSERT INTO products (name, description, price) VALUES 
+                    ('Product 1', 'Description 1', 19.99)",
+				"INSERT INTO images (product_id, image_url) VALUES 
+                    (1, 'path_to_image_1.jpg'), 
+                    (1, 'path_to_image_2.jpg')",
+				"INSERT INTO users (username, email) VALUES 
+                    ('user1', 'user1@example.com')",
+				"INSERT INTO carts (user_id) VALUES (1)",
+				"INSERT INTO cart_items (cart_id, product_id) VALUES (1,1)",
+			];
+			echo "Initial data inserted successfully";
 		} catch(\PDOException $e) {
-			echo "Error creating tables: " .$e->getMessage();
+			echo "Error inserting initial data: " . $e->getMessage();
 		}
 	}
 }
