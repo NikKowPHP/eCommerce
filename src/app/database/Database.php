2ada6f33f35486356d31b67b65ad44dfd948ac31
NikKowPHP <nik.kow@outlook.com>
2024-01-07 08:57:50 +0100

    feat(Database): Create a database class and define construct method with getConnection to establish the connection to the database

---
+++ src/app/database/Database.php
@@ -0,0 +1,25 @@
+<?php
+namespace App;
+
+require_once("../../config/database.php");
+
+class Database
+{
+	private \PDO $pdo;
+
+	public function __construct()
+	{
+		$dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4';
+		try {
+			$this->pdo = new \PDO($dsn, DB_USER, DB_PASSWORD);
+			$this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		} catch (\PDOException $e) {
+			echo "Connection failed: " . $e->getMessage();
+			die();
+		}
+	}
+	public function getConnection(): \PDO
+	{
+		return $this->pdo;
+	}
+}
