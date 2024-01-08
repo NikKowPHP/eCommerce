d90ba7faad3fc14f7092205cfd0bddfbaefec96c
NikKowPHP <nik.kow@outlook.com>
2024-01-08 07:41:18 +0100

    feat(Product): Define setters for Product class

--- src/app/Models/Product.php
+++ src/app/Models/Product.php
@@ -1,25 +1,71 @@
 <?php
+declare(strict_types=1);
 namespace App\Models;
 
-declare(strict_types=1);
+use App\Models\Model;
 
-class Product
+
+class Product extends Model
 {
 	private int $id;
-	private string $title;
+	private string $name;
 	private string $description;
+	private float $price;
 	private array $images;
+	private string $tableName;
 
 	public function __construct(
 		int $id = 0,
-		string $title = '',
+		string $name = '',
 		string $description = '',
+		float $price = 0.0,
 		array $images = []
 	) {
 		$this->id = $id;
-		$this->title = $title;
+		$this->name = $name;
 		$this->description = $description;
 		$this->images = $images;
+		$this->tableName = 'products';
+		parent::__construct();
+	}
+	public function setId(int|string $id):void
+	{
+		$id = (int) $id;
+		$this->id = $id;
+	}
+	public function setName(string $name):void
+	{
+		$this->name = $name;
+	}
+	public function setDescription(string $description):void
+	{
+		$this->description = $description;
+	}
+	public function setPrice(float $price):void
+	{
+		$this->price = $price;
+	}
+	public function getName():string
+	{
+		return $this->name;
+	}
+	public function getDescription():string
+	{
+		return $this->description;
+	}
+	public function getPrice():float
+	{
+		return $this->price;
+	}
+	
+	
+	public function create(array $data): void
+	{
+
+	}
+	protected function getTableName(): string
+	{
+		return $this->tableName;
 	}
 
 }
