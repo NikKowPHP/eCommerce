8dd120652b33444779b1cd78a0f94388db05acfb
NikKowPHP <nik.kow@outlook.com>
2024-01-08 07:46:27 +0100

    feat(ProductController): Introduce show method that retrieves a specific product by id and related images

--- src/app/Controllers/ProductController.php
+++ src/app/Controllers/ProductController.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 namespace App\Controllers;
 
+use App\Models\Image;
+use App\Models\Product;
+
 class ProductController extends AbstractController
 {
 	public function index(): void
@@ -9,9 +12,15 @@ class ProductController extends AbstractController
 		$viewPath = __DIR__ . '/../Views/products.php';
 		$this->includeView($viewPath);
 	}
-	public function show(int $id):void
+	public function show(int $id): void
 	{
+		$product = new Product();
+		$product->read($id);
+		$image = new Image();
+		$images = $image->findAllBy('product_id', $id);
+		print_r($images);
+
 		$viewPath = __DIR__ . '/../Views/product.php';
-		$this->includeView($viewPath);
+		$this->includeView($viewPath, ['product' => $product]);
 	}
 }
