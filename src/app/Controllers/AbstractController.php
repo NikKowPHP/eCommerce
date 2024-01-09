06132976dc86bb9e7e2c9ee7c77f14e9c79c135a
NikKowPHP <nik.kow@outlook.com>
2024-01-09 08:58:38 +0100

    feat(product): Create product view to demostrate the retrieved instances of the specific item

--- src/app/Controllers/AbstractController.php
+++ src/app/Controllers/AbstractController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use App\Traits\ViewPathTrait;
 
-abstract class AbstractController
+abstract class AbstractController 
 {
 	use ViewPathTrait;
 	abstract public function index(): void;
--- src/app/Views/product.php
+++ src/app/Views/product.php
@@ -1,3 +1,11 @@
-<?php
-$id = $_GET['id'];
-echo "<h1>This is a specific product page with id $id</h1>";
+<div class='container'>
+<h2><?= $product->getName() ?></h2>
+<h3><?= $product->getDescription() ?></h3>
+<h3><?= $product->getPrice() ?></h3>
+
+<?php foreach($product->getImages() as $image): ?>
+
+<h3><?= $image->getImageUrl(); ?></h3>
+
+<?php endforeach; ?>
+</div>
