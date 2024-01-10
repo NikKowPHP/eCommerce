9e6f85315589e8c78843a4bb28044ed780ebbbf2
NikKowPHP <nik.kow@outlook.com>
2024-01-10 11:21:59 +0100

    feat(User): Define user class with register method that is triggering saving to the db parent method

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -1,9 +1,39 @@
 <?php
-namespace App;
-
 declare(strict_types=1);
+namespace App\Models;
 
-class User 
+
+class User extends Model
 {
-	
+	private int $id;
+	private string $username;
+	private string $email;
+	private string $password;
+	private string $registrationDate;
+
+	public function __construct(
+		string $username = '',
+		string $email = '',
+		string $password = '',
+		string $registrationDate = '',
+	) {
+		$this->email = $email;
+		$this->username = $username;
+		$this->password = $password;
+		$this->registrationDate = $registrationDate;
+		parent::__construct();
+	}
+
+	public function getTableName(): string
+	{
+		return 'users';
+	}
+	public function register(): void
+	{
+		
+		if($this->save()) {
+			echo 'IT IS SAVED';
+		}
+	}
+
 }
