cf9664b27494b2ce15385b1d323724f8624e4444
NikKowPHP <nik.kow@outlook.com>
2024-01-10 11:25:14 +0100

    feat(RegisterController): Introduce register controller with register method that handles the register logic

---
+++ src/app/Controllers/RegisterController.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+namespace App\Controllers;
+
+use App\Models\User;
+
+class RegisterController extends AbstractController
+{
+	public function register(): void
+	{
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			$username = trim($_POST['username']);
+			$email = trim($_POST['email']);
+			$password = $_POST['password'];
+
+			if (empty($username) || empty($email) || empty($password)) {
+
+			}
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+			}
+			$salt = random_bytes(16);
+
+			$saltedPassword = $password . $salt;
+
+			$hashedPassword = password_hash($saltedPassword, PASSWORD_DEFAULT);
+			$user = new User($username, $email, $hashedPassword);
+			$user->register();
+
+		}
+
+	}
+
+	public function index(): void
+	{
+		$viewPath = __DIR__ . '/../Views/signupForm.php';
+		$this->includeView($viewPath);
+	}
+}
