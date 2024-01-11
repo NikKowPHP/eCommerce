bdec61e1ac315c60dd8e6b79f55f31fec599fc6f
NikKowPHP <nik.kow@outlook.com>
2024-01-11 13:38:28 +0100

    feat(loginController): Utilize auth class and update login method

--- src/app/Controllers/LoginController.php
+++ src/app/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Models\User;
 use App\Utils\Location;
 use App\Utils\SessionManager;
+use App\Utils\Auth;
 
 class LoginController extends AbstractController
 {
@@ -14,29 +15,25 @@ class LoginController extends AbstractController
 			$email = trim($_POST['email']);
 			$inputPassword = $_POST['password'];
 
-			if (empty($email) || empty($inputPassword)) {
+			if (empty($email) || empty($inputPassword) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-			}
-			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
+				// Handle validation errors
+				SessionManager::setFlashMessage('failure', 'Invalid email or password');
+				Location::redirect('/login');
 			}
 
 			$user = new User();
 			$user->read($email, 'email');
-			if ($user->getId())
-				if (password_verify($inputPassword, $user->getPassword())) {
-					SessionManager::startSession();
-					SessionManager::regenerateSessionId();
-					SessionManager::setSessionValue('user_id', $user->getId());
-					SessionManager::setFlashMessage('success', 'You have successfully logged in');
-					Location::redirect('/');
-					// TODO: Session login , regenerate key, create a cookie
-				} else {
-					SessionManager::setFlashMessage('failure', 'The provided credentials have not found, try again');
-					Location::redirect('/login');
 
-				}
-
+			if ($user->getId() && password_verify($inputPassword, $user->getPassword())) {
+				Auth::logIn($user->getId());
+				SessionManager::setFlashMessage('success', 'You have successfully logged in');
+				Location::redirect('/');
+				// TODO:  create a cookie
+			} else {
+				SessionManager::setFlashMessage('failure', 'The provided credentials have not found, try again');
+				Location::redirect('/login');
+			}
 		}
 	}
 	public function index(): void
