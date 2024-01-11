5c6a8d0b1500f4b9e9ae51363cae520d309a60d1
NikKowPHP <nik.kow@outlook.com>
2024-01-11 09:33:19 +0100

    feat(User): Update user model with getters and setters. Define in register method a current timestamp for registration date

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -24,6 +24,61 @@ class User extends Model
 		parent::__construct();
 	}
 
+	public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    // Getter and Setter for 'username'
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): void
+    {
+        $this->username = $username;
+    }
+
+    // Getter and Setter for 'email'
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    // Getter and Setter for 'password'
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+
+    // Getter and Setter for 'registrationDate'
+    public function getRegistrationDate(): string
+    {
+        return $this->registrationDate;
+    }
+
+    public function setRegistrationDate(string $registrationDate): void
+    {
+        $this->registrationDate = $registrationDate;
+    }
+
 	public function getTableName(): string
 	{
 		return 'users';
@@ -31,6 +86,7 @@ class User extends Model
 	public function register(): void
 	{
 		
+		$this->registrationDate = date('Y-m-d H:i:s');
 		if($this->save()) {
 			echo 'IT IS SAVED';
 		}
