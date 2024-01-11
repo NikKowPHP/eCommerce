45ffc26d5aeb1283c3b174068f045224c15afc5d
NikKowPHP <nik.kow@outlook.com>
2024-01-11 09:32:31 +0100

    feat(routes): Add routes for forms and for user creation and authentication post requests

--- src/app/routes.php
+++ src/app/routes.php
@@ -4,5 +4,9 @@ $routes = [
 	'GET /products' => 'ProductController@index',
 	'GET /products/{id}' => 'ProductController@show',
 	'GET /cart' => 'CartController@index',
+	'GET /signup' => 'RegisterController@index',
+	'GET /login' => 'LoginController@index',
+	'POST /users/signup' => 'RegisterController@register',
+	'POST /users/login' => 'LoginController@login',
 ];
 ?>
