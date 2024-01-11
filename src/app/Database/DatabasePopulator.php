f67874f6cf21f184c92077beef1bc78f208107ea
NikKowPHP <nik.kow@outlook.com>
2024-01-11 09:31:22 +0100

    feat(DatabasePopulator): Add registrationDate field to populate the db

--- src/app/Database/DatabasePopulator.php
+++ src/app/Database/DatabasePopulator.php
@@ -29,7 +29,9 @@ class DatabasePopulator
 				"CREATE TABLE IF NOT EXISTS users (
 						id INT AUTO_INCREMENT PRIMARY KEY,
 						username VARCHAR(255) NOT NULL,
-						email VARCHAR(255) NOT NULL
+						email VARCHAR(255) NOT NULL,
+						password VARCHAR(255) NOT NULL,
+						registrationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 				)",
 				"CREATE TABLE IF NOT EXISTS carts (
 						id INT AUTO_INCREMENT PRIMARY KEY,
