9025ae4b2ba626e13b5f7bcd498593c74d857b5a
NikKowPHP <nik.kow@outlook.com>
2024-01-12 08:55:10 +0100

    feat(Auth): update login and loguout with cookies

--- src/app/Utils/Auth.php
+++ src/app/Utils/Auth.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace App\Utils;
 
 use App\Utils\SessionManager;
+
 class Auth
 {
 	public static function isLoggedIn(): bool
@@ -11,11 +12,14 @@ class Auth
 	}
 	public static function logIn(int $userId): void
 	{
+		// TODO: Auth token
 		SessionManager::regenerateSessionId();
 		SessionManager::setSessionValue('user_id', $userId);
+		Cookie::set('user_id', (string) $userId);
 	}
 	public static function logOut(int $userId): void
 	{
+		Cookie::delete('user_id');
 		SessionManager::destroySession();
 	}
 }
