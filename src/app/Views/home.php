4c9f3a27c503fda305a48615e6fa14b952b73bd6
NikKowPHP <nik.kow@outlook.com>
2024-01-15 10:52:51 +0100

    feat(home): Update home page with session

--- src/app/Views/home.php
+++ src/app/Views/home.php
@@ -1,3 +1,10 @@
 <?php
+use App\Utils\SessionManager;
+$successMessage = SessionManager::getFlashMessage('success');
+if($successMessage) {
+	echo "<div>$successMessage</div>";
+}
+echo SessionManager::getSessionValue('user_id');
 
 echo " THIS IS HOME PAGE ";
+
