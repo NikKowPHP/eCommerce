77b3ceb2cd26af0c02d739f70619a55a16546055
NikKowPHP <nik.kow@outlook.com>
2024-01-15 10:55:14 +0100

    feat(products view): Update products view with a basic table

--- src/app/Views/admin/products.php
+++ src/app/Views/admin/products.php
@@ -1,11 +1,13 @@
-<div class="container col-md-6">
+<div class="container col-md-12">
+	<div><a class="btn btn-primary m-4" href="/admin/products/create">Add a new product</a></div>
+
 	<table class="table">
 		<thead class="thead-dark">
 			<tr>
 				<th scope="col">#</th>
-				<th scope="col">First</th>
-				<th scope="col">Last</th>
-				<th scope="col">Handle</th>
+				<th scope="col">Name</th>
+				<th scope="col">Description</th>
+				<th scope="col">Actions</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -15,10 +17,10 @@
 					<td><?= $product->getName() ?></td>
 					<td><?= $product->getDescription() ?></td>
 					<td>
-						<a href="/admin/products/remove/<?= $product_id ?>">Remove</a>
+						<a class="btn btn-danger w-75 m-0" href="/admin/products/remove/<?= $product_id ?>">Remove</a>
 					</td>
 					<td>
-						<a href="/admin/products/edit?id=<?= $product_id ?>">Edit</a>
+						<a class="btn btn-secondary" href="/admin/products/edit?id=<?= $product_id ?>">Edit</a>
 					</td>
 				</tr>
 
