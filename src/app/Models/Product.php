a4781e576ee7b55d04dc6cf4be65f78db2a460e0
NikKowPHP <nik.kow@outlook.com>
2024-01-15 10:53:42 +0100

    feat(Product): Introduce write method to call the save method with hidden props

--- src/app/Models/Product.php
+++ src/app/Models/Product.php
@@ -15,58 +15,66 @@ class Product extends Model
 	private string $tableName;
 
 	public function __construct(
-		int $id = 0,
 		string $name = '',
 		string $description = '',
 		float $price = 0.0,
-		array $images = []
 	) {
-		$this->id = $id;
 		$this->name = $name;
 		$this->description = $description;
-		$this->images = $images;
+		$this->price = $price;
 		$this->tableName = 'products';
 		parent::__construct();
 	}
-	public function setId(int|string $id):void
+	public function setId(int|string $id): void
 	{
 		$id = (int) $id;
 		$this->id = $id;
 	}
-	public function setName(string $name):void
+
+	public function setName(string $name): void
 	{
 		$this->name = $name;
 	}
-	public function setDescription(string $description):void
+	public function setDescription(string $description): void
 	{
 		$this->description = $description;
 	}
-	public function setPrice(float $price):void
+	public function setPrice(float $price): void
 	{
 		$this->price = $price;
 	}
-	public function setImages(array $images):void
+	public function setImages(array $images): void
 	{
 		$this->images = $images;
 	}
-	public function getImages():array
+	public function getImages(): array
 	{
 		return $this->images;
 	}
-	public function getName():string
+	public function getName(): string
 	{
 		return $this->name;
 	}
-	public function getDescription():string
+	public function getDescription(): string
 	{
 		return $this->description;
 	}
-	public function getPrice():float
+	public function getPrice(): float
 	{
 		return $this->price;
 	}
-	
-	
+	public function getId(): int
+	{
+		return $this->id;
+	}
+
+	public function write(): ?int
+	{
+		$this->setHiddenProps(['images']);
+		return $this->save();
+	}
+
+
 	public function create(array $data): void
 	{
 
