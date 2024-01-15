44ab0d84cada32855c61fb26c86ffd5146a43f91
NikKowPHP <nik.kow@outlook.com>
2024-01-15 10:47:13 +0100

    refactor(File): Update upload method, add extension attribute

--- src/app/Models/File.php
+++ src/app/Models/File.php
@@ -5,29 +5,43 @@ namespace App\Models;
 
 class File extends Model
 {
-	public function getTableName():string
+	public function getTableName(): string
 	{
 		return 'files';
 	}
-	public static function upload(string $inputName, string $destinationPath):?string
+	public static function upload(string $inputName, string $destination, array $allowedExtensions = []): ?string
 	{
-		if(isset($_FILES[$inputName])) {
-			$uploadedFile = $_FILES[$inputName];
+		if (!isset($_FILES[$inputName]))
+			return null;
 
-			if($uploadedFile['error'] === UPLOAD_ERR_OK) {
-				$tempPath = $uploadedFile['tmp_name'];
-				$filename = basename($uploadedFile['name']);
-				$destination = rtrim($destinationPath, '/'). '/' . $filename;
-				if(move_uploaded_file($tempPath, $destination)) {
-					return $destination;
-				}
+		$file = $_FILES[$inputName];
+		$fileName = $file['name'];
+		$fileTmpName = $file['tmp_name'];
+		$fileSize = $file['size'];
+		$fileError = $file['error'];
+		$fileType = $file['type'];
+
+		if ($fileError !== 0) {
+			echo 'file has not uploaded';
+			return null;
+		}
+		if (!empty($allowedExtensions)) {
+			$fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+			if (!in_array(strtolower($fileExtension), $allowedExtensions)) {
+				return null;
 			}
 		}
-		return null;
+		// Generate a unique filename to prevent overwriting
+		$uniqueFileName = uniqid() . '_' . $fileName;
+
+		// Move the uploaded file to the destination directory
+		$destinationPath = rtrim($destination, '/') . '/' . $uniqueFileName;
+		move_uploaded_file($fileTmpName, $destinationPath);
+		return $uniqueFileName;
 	}
 	public static function delete(string $filePath): bool
 	{
-		if(file_exists($filePath)) {
+		if (file_exists($filePath)) {
 			return unlink($filePath);
 		}
 		return false;
