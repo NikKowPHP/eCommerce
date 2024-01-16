bea7d4e7871c434ade3607d6fb66c510ebdb6c11
NikKowPHP <nik.kow@outlook.com>
2024-01-16 08:50:56 +0100

    feat(product view): Render product info with images and links

--- src/app/Views/product.php
+++ src/app/Views/product.php
@@ -4,8 +4,13 @@
 <h3><?= $product->getPrice() ?></h3>
 
 <?php foreach($product->getImages() as $image): ?>
-
-<h3><?= $image->getImageUrl(); ?></h3>
+	<img width="100%" src="/images/<?= $image->getImageUrl() ?>" alt="">
 
 <?php endforeach; ?>
+
+<div class="mt-5 d-flex justify-content-center">
+	<a href="/admin/product/edit/<?= $product->getId() ?>" class="btn btn-secondary">Edit</a>
+	<a href="/admin/product/delete/<?= $product->getId() ?>" class="btn btn-danger">Delete</a>
+</div>
+
 </div>
