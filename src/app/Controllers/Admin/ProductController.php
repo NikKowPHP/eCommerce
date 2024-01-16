9f4a5ab9cfbd3bda4cae0a9345a360cf0a801ff0
NikKowPHP <nik.kow@outlook.com>
2024-01-16 11:07:55 +0100

    feat(ProductController): Introduce update method to update the product and upload images

--- src/app/Controllers/Admin/ProductController.php
+++ src/app/Controllers/Admin/ProductController.php
@@ -6,6 +6,8 @@ use App\Controllers\Admin\AbstractAdminController;
 use App\Models\Image;
 use App\Models\Product;
 use App\Services\ProductService;
+use App\Utils\Location;
+use App\Utils\SessionManager;
 
 class ProductController extends AbstractAdminController
 {
@@ -50,13 +52,44 @@ class ProductController extends AbstractAdminController
 		}
 
 	}
-	public function edit(): void
+	// Renders edit view
+	public function edit($id): void
 	{
-
+		$viewPath = __DIR__ . '/../../Views/admin/product/editProductView.php';
+		$product = new Product();
+		$product->read($id);
+		$image = new Image();
+		$images = $image->findAllBy('productId', $id);
+		$product->setImages($images);
+		$this->includeView($viewPath, ['product' => $product]);
 	}
-	public function update(): void
+	// Updates the product
+	public function update(): bool
 	{
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			$productId = (int) $_POST['id'];
 
+			if (!(new Product())->read($productId)) {
+				return false;
+			}
+			$name = $_POST['name'];
+			$description = $_POST['description'];
+			$price = (float) $_POST['price'];
+			$product = new Product($name, $description, $price);
+			$product->setId($productId);
+			$product->setHiddenProps('images');
+
+			if ($this->productService->updateProduct($product)) {
+				SessionManager::setFlashMessage('success', "Product {$product->getName()} has been updated");
+				Location::redirect('/admin/product/edit/' . $product->getId());
+				return true;
+			}
+			SessionManager::setFlashMessage('failure', "Product {$product->getName()} has not been updated, try again");
+			Location::redirect('/admin/product/edit/' . $product->getId());
+			return false;
+		}
+		Location::redirect('/');
+		return false;
 	}
 	public function destroy(): void
 	{
