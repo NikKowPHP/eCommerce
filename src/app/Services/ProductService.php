9f0428ae778c246fd39313a7a8060eff7c928002
NikKowPHP <nik.kow@outlook.com>
2024-01-16 11:09:55 +0100

    feat(ProductService): Introduce update product method to handle the business logic of the product updation

--- src/app/Services/ProductService.php
+++ src/app/Services/ProductService.php
@@ -15,39 +15,61 @@ class ProductService
 			$pdo = (new Product())->getDatabase()->getConnection();
 			$pdo->beginTransaction();
 
-			if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-				$name = $_POST['name'];
-				$description = $_POST['description'];
-				$price = (float) $_POST['price'];
-				$product = new Product();
-				if ($product->read($name, 'name')) {
+			$product = new Product();
+			if ($product->read($name, 'name')) {
+				$pdo->rollBack();
+				return null;
+			}
+
+			$product = new Product($name, $description, $price);
+
+			// Write the product to the database
+			if ($productId = $product->write()) {
+				$product->setId($productId);
+				// handle file upload
+				$uploadedFileName = File::upload('file', IMAGE_UPLOAD_PATH);
+				if ($uploadedFileName !== null) {
+					$image = new Image($productId, $uploadedFileName);
+					$image->write();
+				} else {
 					$pdo->rollBack();
 					return null;
 				}
-
-				$product = new Product($name, $description, $price);
-
-				// Write the product to the database
-				if ($productId = $product->write()) {
-					$product->setId($productId);
-					// handle file upload
-					$uploadedFileName = File::upload('file', IMAGE_UPLOAD_PATH);
-					if ($uploadedFileName !== null) {
-						$image = new Image($productId, $uploadedFileName);
-						$image->write();
-					} else {
-						$pdo->rollBack();
-						return null;
-					}
-				}
-				$pdo->commit();
-				return $productId;
 			}
+			$pdo->commit();
+			return $productId;
 
 		} catch (\PDOException $e) {
 			$pdo->rollBack();
 			echo 'Transaction failed: ' . $e->getMessage();
 			return null;
+		}
+	}
+
+	public function updateProduct(Product $product): bool
+	{
+		try {
+			$pdo = $product->getDatabase()->getConnection();
+			$pdo->beginTransaction();
+
+			$result = $product->update();
+			if ($result) {
+				$uploadedFileName = File::upload('file', IMAGE_UPLOAD_PATH);
+
+				if ($uploadedFileName !== null) {
+					$image = new Image($product->getId(), $uploadedFileName);
+					$image->write();
+				} else {
+					$pdo->rollBack();
+					return false;
+				}
+			}
+			$pdo->commit();
+			return true;
+		} catch (\PDOException $e) {
+			$pdo->rollBack();
+			echo 'Transaction failed: ' . $e->getMessage();
+			return false;
 		}
 	}
 }
