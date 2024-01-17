182a4a004fd8e6a44dc5516c5d2be1e7aee4e5a0
NikKowPHP <nik.kow@outlook.com>
2024-01-17 15:12:37 +0100

    feat(routes): Add more routes

--- src/app/routes.php
+++ src/app/routes.php
@@ -10,22 +10,24 @@ $routes = [
 	'GET /products/{id}' => 'ProductController@show',
 	'GET /cart' => 'CartController@index',
 
-	// ADMIN Views 
 	'GET /admin/products' => 'ProductController@index',
 	'GET /admin/product/{id}' => 'ProductController@show',
-	'GET /admin/users' => 'UserController@index',
-	'GET /admin/carts' => 'CartController@index',
-
-	// Admin Crud
 	'GET /admin/product/create' => 'ProductController@create',
 	'POST /admin/product/store' => 'ProductController@store',
-	'GET /admin/product/remove/{id}' => 'ProductController@destroy',
+	'POST /admin/product/{id}' => 'ProductController@destroy',
 	'GET /admin/product/edit/{id}' => 'ProductController@edit',
 	'POST /admin/product/update' => 'ProductController@update',
 
-	'POST /admin/users/create/{id}' => 'UserController@index',
-	'GET /admin/users/remove/{id}' => 'UserController@destroy',
+	'GET /admin/users' => 'UserController@index',
+	'POST /admin/users' => 'UserController@destroy',
+	'GET /admin/user/{id}' => 'UserController@show',
+	'GET /admin/user/create' => 'UserController@create',
+	'POST /admin/user/create' => 'UserController@store',
+	'GET /admin/user/edit/{id}' => 'UserController@edit',
+	'POST /admin/user/edit/{id}' => 'UserController@update',
+
 	'POST /admin/carts' => 'CartController@index',
+	'GET /admin/carts' => 'CartController@index',
 
 	'POST /users/signup' => 'RegisterController@register',
 	'POST /users/login' => 'LoginController@login',
