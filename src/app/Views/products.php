61f064ff9d7b19c2457f8af7a31c1bbbef470446
NikKowPHP <nik.kow@outlook.com>
2024-01-19 15:43:04 +0100

    feat(products): Display remove or add to cart btn based on userItems cart

--- src/app/Views/products.php
+++ src/app/Views/products.php
@@ -1,30 +1,41 @@
 <div class="container">
 
 	<?php foreach ($products as $product): ?>
+		<?php
+		$isProductInCart = false;
 
+		foreach ($userItems as $userItem) {
+			if ($userItem->getProductId() === $product->getId()) {
+				$isProductInCart = true;
+				break;
+			}
+		}
+		?>
 		<div class="col-md-6">
 			<h3>
 				<?= $product->getName() ?>
 			</h3>
 			<p>
 				<?= $product->getDescription() ?>
-
 			</p>
 			<?php if ($product->getThumbnail()): ?>
-				<img src="/images/<?= $product->getThumbnail() ?>" alt="">
+				<img width="100%" src="/images/<?= $product->getThumbnail() ?>" alt="">
 			<?php endif; ?>
 
-			<form action="/products" method="POST">
-				<input type="hidden" name="productId" value=<?= $product->getId() ?>>
-				<button type="submit">Add to cart</button>
-			</form>
+			<?php if ($isProductInCart): ?>
+				<form action="/products/remove" method="GET">
+					<input type="hidden" name="productId" value=<?= $product->getId() ?>>
+					<input type="hidden" name="_method" value="POST">
+					<button type="submit">Remove from cart</button>
+				</form>
 
-			
-			<form action="/products/remove" method="GET">
-				<input type="hidden" name="productId" value=<?= $product->getId() ?>>
-				<input type="hidden" name="_method" value="POST" >
-				<button type="submit">Remove from cart</button>
-			</form>
+
+			<?php else: ?>
+				<form action="/products" method="POST">
+					<input type="hidden" name="productId" value=<?= $product->getId() ?>>
+					<button type="submit">Add to cart</button>
+				</form>
+			<?php endif; ?>
 		</div>
 
 	<?php endforeach; ?>
