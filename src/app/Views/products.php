8e548b800e0a5f483430edba671fd87e4ba7fc91
NikKowPHP <nik.kow@outlook.com>
2024-01-20 11:33:43 +0100

    refactor(products): Render cart items quantity of the user's cart, update classes

--- src/app/Views/products.php
+++ src/app/Views/products.php
@@ -3,20 +3,19 @@
 
 		<?php foreach ($products as $product): ?>
 			<?php
-			$isProductInCart = false;
+			$isProductInCart = $isProductInUserCart($product->getId());
 
-			foreach ($userItems as $userItem) {
-				if ($userItem->getProductId() === $product->getId()) {
-					$isProductInCart = true;
-					break;
-				}
+			$userCartItemQuantity = null;
+
+			if ($isProductInCart) {
+				$userItem = $getUserItem($product->getId());
+				$userCartItemQuantity = $userItem->getQuantity();
 			}
+
 			?>
 
 			<div class="card col-md-6 mb-3 mx-4">
-				<?php if ($product->getThumbnail()): ?>
-					<img class="card-img-top" src="/images/<?= $product->getThumbnail() ?>" alt="Card image cap">
-				<?php endif; ?>
+					<img class="card-img-top" src="<?= $product->getThumbnail() ?>" alt="product image">
 				<div class="card-body pb-4 px-4 ">
 					<h5 class="card-title">
 						<?= $product->getName() ?>
@@ -25,22 +24,26 @@
 						<?= $product->getDescription() ?>
 					</p>
 					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-					<p class="card-text"><small class="text-muted">
-							<?= $userItem->getQuantity() ?>
-						</small></p>
 				</div>
 				<div class="card-footer pb-4">
 					<?php if ($isProductInCart): ?>
 						<form action="/products/remove" method="GET">
 							<input type="hidden" name="productId" value=<?= $product->getId() ?>>
 							<input type="hidden" name="_method" value="POST">
-							<button class="btn btn-danger" type="submit">Remove from cart</button>
+
+							<div class="form__footer-control d-flex justify-content-center">
+								<input class="w-25 form-control m-2" type="number" name="quantity" value="<?= $userCartItemQuantity ?>">
+								<button class="btn btn-danger" type="submit">Remove from cart</button>
+							</div>
 						</form>
 
 					<?php else: ?>
 						<form action="/products" method="POST">
 							<input type="hidden" name="productId" value=<?= $product->getId() ?>>
 							<button class="btn btn-primary" type="submit">Add to cart</button>
+							<label>
+								<input class="w-50%  form-control" type="number" name="quantity" value="<?= $userCartItemQuantity ?>">
+							</label>
 						</form>
 					<?php endif; ?>
 				</div>
