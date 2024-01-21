e9942c2b5caaea0ebbd4a7569554e065c2b3b6d2
NikKowPHP <nik.kow@outlook.com>
2024-01-21 09:50:11 +0100

    refactor(ProductController):  Move generic methods to the trait class

--- src/app/Controllers/ProductController.php
+++ src/app/Controllers/ProductController.php
@@ -6,10 +6,12 @@ use App\Models\CartItem;
 use App\Models\Image;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Traits\CartItemOperationsTrait;
 use App\Utils\Auth;
 
 class ProductController extends AbstractController
 {
+	use CartItemOperationsTrait;
 	public function index(): void
 	{
 		$product = new Product();
@@ -22,24 +24,12 @@ class ProductController extends AbstractController
 			return $this->isProductInCart($productId, $userCartItems);
 		};
 
-		$getUserItem = function (int $productId) use ($userCartItems): ?CartItem{
+		$getUserItem = function (int $productId) use ($userCartItems): ?CartItem {
 			return $this->getUserItem($productId, $userCartItems);
 		};
 
 		$viewPath = __DIR__ . '/../Views/products.php';
 		$this->includeView($viewPath, ['products' => $products, 'userItems' => $userCartItems, 'isProductInUserCart' => $isProductInUserCart, 'getUserItem' => $getUserItem]);
-	}
-
-	private function getUserItem(int $productId, ?array $userCartItems): ?CartItem
-	{
-		if ($userCartItems !== null) {
-			foreach ($userCartItems as $userItem) {
-				if ($userItem->getProductId() === $productId) {
-					return $userItem;
-				}
-			}
-		}
-		return null;
 	}
 
 	private function isProductInCart(int $productId, ?array $userCartItems): bool
@@ -54,10 +44,6 @@ class ProductController extends AbstractController
 		return false;
 	}
 
-	private function getUserCartItems(Cart $userCart): ?array
-	{
-		return (new CartItem)->findAllBy('cartId', $userCart->getId()) ?? null;
-	}
 	private function updateUserCartItemsQuantity(?array $userCartItems): void
 	{
 		if ($userCartItems !== null) {
