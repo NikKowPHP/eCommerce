ed696ed8dc28b6e0d0606f35d5dcf26b88c275b0
NikKowPHP <nik.kow@outlook.com>
2024-01-21 09:47:56 +0100

    feat(CartItemOperationsTrait): Introduce a trait for cart item operations to use across mutliple classes

---
+++ src/app/Traits/CartItemOperationsTrait.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+namespace App\Traits;
+
+use App\Models\CartItem;
+use App\Models\Cart;
+
+trait CartItemOperationsTrait
+{
+	private function getUserItem(int $productId, ?array $userCartItems): ?CartItem
+	{
+		if ($userCartItems !== null) {
+			foreach ($userCartItems as $userItem) {
+				if ($userItem->getProductId() === $productId) {
+					$userItem->updateQuantity();
+					return $userItem;
+				}
+			}
+		}
+		return null;
+	}
+	private function getUserCartItems(Cart $userCart): ?array
+	{
+		return (new CartItem)->findAllBy('cartId', $userCart->getId()) ?? null;
+	}
+
+}
