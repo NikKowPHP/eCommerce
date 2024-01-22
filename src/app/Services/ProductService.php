020d0dd718064aa37003bb7442828d99f12d78fd
NikKowPHP <nik.kow@outlook.com>
2024-01-22 10:14:55 +0100

    refactor(ProductService): Update product service class based on dependency injection

--- src/app/Services/ProductService.php
+++ src/app/Services/ProductService.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 namespace App\Services;
 
+use App\Database\Database;
+
 require_once(__DIR__ . '/../../config/constants.php');
 use App\Models\Image;
 use App\Models\File;
@@ -9,19 +11,24 @@ use App\Models\Product;
 
 class ProductService
 {
+	private Database $database;
+	public function __construct(Database $database)
+	{
+		$this->database = $database;
+	}
 	public function createProduct(string $name, string $description, float $price): ?int
 	{
 		try {
-			$pdo = (new Product())->getDatabase()->getConnection();
+			$pdo = (new Product($this->database))->getDatabase()->getConnection();
 			$pdo->beginTransaction();
 
-			$product = new Product();
+			$product = new Product($this->database);
 			if ($product->read($name, 'name')) {
 				$pdo->rollBack();
 				return null;
 			}
 
-			$product = new Product($name, $description, $price);
+			$product = new Product($this->database, $name, $description, $price);
 
 			// Write the product to the database
 			if ($productId = $product->write()) {
@@ -29,7 +36,7 @@ class ProductService
 				// handle file upload
 				$uploadedFileName = File::upload('file', IMAGE_UPLOAD_PATH);
 				if ($uploadedFileName !== null) {
-					$image = new Image($productId, $uploadedFileName);
+					$image = new Image($this->database, $productId, $uploadedFileName);
 					$image->write();
 				} else {
 					$pdo->rollBack();
@@ -57,7 +64,7 @@ class ProductService
 				$uploadedFileName = File::upload('file', IMAGE_UPLOAD_PATH);
 
 				if ($uploadedFileName !== null) {
-					$image = new Image($product->getId(), $uploadedFileName);
+					$image = new Image($this->database, $product->getId(), $uploadedFileName);
 					$image->write();
 				} else {
 					$pdo->rollBack();
